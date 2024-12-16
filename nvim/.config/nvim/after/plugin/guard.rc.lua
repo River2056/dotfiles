d62b11622192224049dfe933a714475b7e96a332
Kevin_Tung <chen0625tung@gmail.com>
2024-12-16 10:33:36 +0800

    modified

--- nvim/.config/nvim/after/plugin/guard.rc.lua
+++ nvim/.config/nvim/after/plugin/guard.rc.lua
@@ -2,17 +2,21 @@ local ft = require("guard.filetype")
 
 ft("python"):fmt("black")
 ft("lua"):fmt("lsp"):append("stylua")
-ft('typescript,javascript,typescriptreact'):fmt('prettier')
+-- ft('typescript,javascript,typescriptreact'):fmt('prettier')
+ft("typescript,typescriptreact"):fmt("eslint_d")
+-- ft('html'):fmt('html-lsp')
+-- ft('html'):fmt("lsp"):append("prettier")
 ft("cs"):fmt("lsp"):append("csharpier")
 ft("c"):fmt("lsp"):append("clang-format")
+ft("cpp"):fmt("lsp"):append("clang-format")
 ft("go"):fmt("gofumpt")
 ft("zig"):fmt("zigfmt")
 ft("rs"):fmt("rustfmt")
 ft("nix"):fmt("nixfmt")
 
 vim.g.guard_config = {
-    -- the only options for the setup function
-    fmt_on_save = true,
-    -- Use lsp if no formatter was defined for this filetype
-    lsp_as_default_formatter = false,
+	-- the only options for the setup function
+	fmt_on_save = true,
+	-- Use lsp if no formatter was defined for this filetype
+	lsp_as_default_formatter = false,
 }
