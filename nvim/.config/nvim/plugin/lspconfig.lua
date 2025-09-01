027f2588252a50380ca1a8127700b36f277c885c
Kevin_Tung <chen0625tung@gmail.com>
2025-09-01 20:24:08 +0800

    mac-bok

--- nvim/.config/nvim/plugin/lspconfig.lua
+++ nvim/.config/nvim/plugin/lspconfig.lua
@@ -5,96 +5,96 @@ local util = require("lspconfig.util")
 local protocol = require("vim.lsp.protocol")
 
 protocol.CompletionItemKind = {
-	"", -- Text
-	"", -- Method
-	"", -- Function
-	"", -- Constructor
-	"", -- Field
-	"", -- Variable
-	"", -- Class
-	"ﰮ", -- Interface
-	"", -- Module
-	"", -- Property
-	"", -- Unit
-	"", -- Value
-	"", -- Enum
-	"", -- Keyword
-	"﬌", -- Snippet
-	"", -- Color
-	"", -- File
-	"", -- Reference
-	"", -- Folder
-	"", -- EnumMember
-	"", -- Constant
-	"", -- Struct
-	"", -- Event
-	"ﬦ", -- Operator
-	"", -- TypeParameter
+    "", -- Text
+    "", -- Method
+    "", -- Function
+    "", -- Constructor
+    "", -- Field
+    "", -- Variable
+    "", -- Class
+    "ﰮ", -- Interface
+    "", -- Module
+    "", -- Property
+    "", -- Unit
+    "", -- Value
+    "", -- Enum
+    "", -- Keyword
+    "﬌", -- Snippet
+    "", -- Color
+    "", -- File
+    "", -- Reference
+    "", -- Folder
+    "", -- EnumMember
+    "", -- Constant
+    "", -- Struct
+    "", -- Event
+    "ﬦ", -- Operator
+    "", -- TypeParameter
 }
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	underline = true,
-	update_in_insert = false,
-	virtual_text = { spacing = 4, prefix = "●" },
-	severity_sort = true,
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 4, prefix = "●" },
+    severity_sort = true,
 })
 
 -- Diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 vim.diagnostic.config({
-	virtual_text = {
-		prefix = "●",
-	},
-	update_in_insert = true,
-	float = {
-		source = "always", -- Or "if_many"
-	},
+    virtual_text = {
+        prefix = "●",
+    },
+    update_in_insert = true,
+    float = {
+        source = "always", -- Or "if_many"
+    },
 })
 
 -- common lsp configs
 for _, server in ipairs(lsp.servers) do
-	local opts = {
-		on_attach = lsp.on_attach,
-		capabilities = lsp.capabilities,
-	}
+    local opts = {
+        on_attach = lsp.on_attach,
+        capabilities = lsp.capabilities,
+    }
 
-	-- non linux/mac system, will return \ or \\
-	if package.config:sub(1, 1) ~= "/" then
-		if server == "cmake" then
-			opts["cmd"] = { "cmake-language-server" }
-		end
+    -- non linux/mac system, will return \ or \\
+    if package.config:sub(1, 1) ~= "/" then
+        if server == "cmake" then
+            opts["cmd"] = { "cmake-language-server" }
+        end
 
-		if server == "kotlin_language_server" then
-			opts["cmd"] = { "kotlin-language-server" }
-		end
+        if server == "kotlin_language_server" then
+            opts["cmd"] = { "kotlin-language-server" }
+        end
 
-		if server == "powershell_es" then
-			opts = { bundle_path = c.powershell_es_path }
-		end
-	end
+        if server == "powershell_es" then
+            opts = { bundle_path = c.powershell_es_path }
+        end
+    end
 
-	nvim_lsp[server].setup(opts)
+    nvim_lsp[server].setup(opts)
 end
 
 local function set_python_path(path)
-	local clients = vim.lsp.get_clients({
-		bufnr = vim.api.nvim_get_current_buf(),
-		name = "basedpyright",
-	})
-	for _, client in ipairs(clients) do
-		if client.settings then
-			client.settings.python = vim.tbl_deep_extend("force", client.settings.python or {}, { pythonPath = path })
-		else
-			client.config.settings =
-				vim.tbl_deep_extend("force", client.config.settings, { python = { pythonPath = path } })
-		end
-		client.notify("workspace/didChangeConfiguration", { settings = nil })
-	end
+    local clients = vim.lsp.get_clients({
+        bufnr = vim.api.nvim_get_current_buf(),
+        name = "basedpyright",
+    })
+    for _, client in ipairs(clients) do
+        if client.settings then
+            client.settings.python = vim.tbl_deep_extend("force", client.settings.python or {}, { pythonPath = path })
+        else
+            client.config.settings =
+                vim.tbl_deep_extend("force", client.config.settings, { python = { pythonPath = path } })
+        end
+        client.notify("workspace/didChangeConfiguration", { settings = nil })
+    end
 end
 
 --[[ nvim_lsp.basedpyright.setup({
@@ -141,25 +141,25 @@ end
 local libs = vim.api.nvim_get_runtime_file("", true)
 table.insert(libs, "${3rd}/love2d/library")
 nvim_lsp.lua_ls.setup({
-	on_attach = lsp.on_attach,
-	capabilities = lsp.capabilities,
-	settings = {
-		Lua = {
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
+    on_attach = lsp.on_attach,
+    capabilities = lsp.capabilities,
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
 
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = libs,
-				checkThirdParty = false,
-			},
-		},
-	},
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = libs,
+                checkThirdParty = false,
+            },
+        },
+    },
 })
 
-nvim_lsp.ts_ls.setup({
+--[[ nvim_lsp.ts_ls.setup({
 	-- on_attach = lsp.on_attach,
 	init_options = { hostInfo = "neovim" },
 	filetypes = {
@@ -244,7 +244,7 @@ nvim_lsp.ts_ls.setup({
 			})
 		end, {})
 	end,
-})
+}) ]]
 
 -- If you are using mason.nvim, you can get the ts_plugin_path like this
 -- For Mason v1,
@@ -260,36 +260,36 @@ nvim_lsp.ts_ls.setup({
 local vue_language_server_path = "/Users/kevintung/.nvm/versions/node/v23.8.0/bin/vue-language-server"
 local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
 local vue_plugin = {
-	name = "@vue/typescript-plugin",
-	location = vue_language_server_path,
-	languages = { "vue" },
-	configNamespace = "typescript",
+    name = "@vue/typescript-plugin",
+    location = vue_language_server_path,
+    languages = { "vue" },
+    configNamespace = "typescript",
 }
 local vtsls_config = {
-	settings = {
-		vtsls = {
-			tsserver = {
-				globalPlugins = {
-					vue_plugin,
-				},
-			},
-		},
-		vue = {
-			format = {
-				enable = true,
-				options = {
-					printWidth = 120,
-					singleQuote = true,
-					trailingComma = "none",
-					tabWidth = 2,
-					useTabs = false,
-					bracketSpacing = true,
-					arrowParens = "always",
-					htmlWhitespaceSensitivity = "strict",
-					endOfLine = "lf",
-					semi = true,
-					quoteProps = "as-needed",
-					--[[ plugins = { "prettier-plugin-organize-imports" },
+    settings = {
+        vtsls = {
+            tsserver = {
+                globalPlugins = {
+                    vue_plugin,
+                },
+            },
+        },
+        vue = {
+            format = {
+                enable = true,
+                options = {
+                    printWidth = 120,
+                    singleQuote = true,
+                    trailingComma = "none",
+                    tabWidth = 2,
+                    useTabs = false,
+                    bracketSpacing = true,
+                    arrowParens = "always",
+                    htmlWhitespaceSensitivity = "strict",
+                    endOfLine = "lf",
+                    semi = true,
+                    quoteProps = "as-needed",
+                    --[[ plugins = { "prettier-plugin-organize-imports" },
 					overrides = {
 						{
 							files = "*.vue",
@@ -354,62 +354,93 @@ local vtsls_config = {
 							},
 						},
 					}, ]]
-				},
-			},
-		},
-	},
-	filetypes = tsserver_filetypes,
+                },
+            },
+        },
+    },
+    filetypes = tsserver_filetypes,
 }
 
+local function get_diagnostic_at_cursor()
+    local cur_buf = api.nvim_get_current_buf()
+    local line, col = unpack(api.nvim_win_get_cursor(0))
+    local entrys = diagnostic.get(cur_buf, { lnum = line - 1 })
+    local res = {}
+    for _, v in pairs(entrys) do
+        if v.col <= col and v.end_col >= col then
+            table.insert(res, {
+                code = v.code,
+                message = v.message,
+                range = {
+                    ["start"] = {
+                        character = v.col,
+                        line = v.lnum,
+                    },
+                    ["end"] = {
+                        character = v.end_col,
+                        line = v.end_lnum,
+                    },
+                },
+                severity = v.severity,
+                source = v.source or nil,
+            })
+        end
+    end
+    return res
+end
+
 local ts_ls_config = {
-	init_options = {
-		plugins = {
-			vue_plugin,
-		},
-	},
-	filetypes = tsserver_filetypes,
+    init_options = {
+        hostInfo = "neovim",
+        plugins = {
+            vue_plugin,
+        },
+    },
+    filetypes = tsserver_filetypes,
+    cmd = { "typescript-language-server", "--stdio" },
+    on_attach = lsp.on_attach,
 }
 
 -- If you are on most recent `nvim-lspconfig`
 local vue_ls_config = {
-	on_init = function(client)
-		client.handlers["tsserver/request"] = function(_, result, context)
-			local ts_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "ts_ls" })
-			local vtsls_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
-			local clients = {}
+    on_init = function(client)
+        client.handlers["tsserver/request"] = function(_, result, context)
+            local ts_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "ts_ls" })
+            local vtsls_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+            local clients = {}
 
-			vim.list_extend(clients, ts_clients)
-			vim.list_extend(clients, vtsls_clients)
+            vim.list_extend(clients, ts_clients)
+            vim.list_extend(clients, vtsls_clients)
 
-			if #clients == 0 then
-				vim.notify(
-					"Could not find `vtsls` or `ts_ls` lsp client, `vue_ls` would not work without it.",
-					vim.log.levels.ERROR
-				)
-				return
-			end
-			local ts_client = clients[1]
+            if #clients == 0 then
+                vim.notify(
+                    "Could not find `vtsls` or `ts_ls` lsp client, `vue_ls` would not work without it.",
+                    vim.log.levels.ERROR
+                )
+                return
+            end
+            local ts_client = clients[1]
 
-			local param = unpack(result)
-			local id, command, payload = unpack(param)
-			ts_client:exec_cmd({
-				title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-				command = "typescript.tsserverRequest",
-				arguments = {
-					command,
-					payload,
-				},
-			}, { bufnr = context.bufnr }, function(_, r)
-				local response = r and r.body
-				-- TODO: handle error or response nil here, e.g. logging
-				-- NOTE: Do NOT return if there's an error or no response, just return nil back to the vue_ls to prevent memory leak
-				local response_data = { { id, response } }
+            local param = unpack(result)
+            local id, command, payload = unpack(param)
+            ts_client:exec_cmd({
+                title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+                command = "typescript.tsserverRequest",
+                arguments = {
+                    command,
+                    payload,
+                },
+            }, { bufnr = context.bufnr }, function(_, r)
+                local response = r and r.body
+                -- TODO: handle error or response nil here, e.g. logging
+                -- NOTE: Do NOT return if there's an error or no response, just return nil back to the vue_ls to prevent memory leak
+                local response_data = { { id, response } }
 
-				---@diagnostic disable-next-line: param-type-mismatch
-				client:notify("tsserver/response", response_data)
-			end)
-		end
-	end,
+                ---@diagnostic disable-next-line: param-type-mismatch
+                client:notify("tsserver/response", response_data)
+            end)
+        end
+    end,
 }
 -- nvim 0.11 or above
 vim.lsp.config("vtsls", vtsls_config)
@@ -433,74 +464,74 @@ vim.lsp.enable({ "ts_ls", "vue_ls" }) -- If using `ts_ls` replace `vtsls` to `ts
 --[[ htmlFormatter["html.format.wrapLineLength"] = 180
 htmlFormatter["html.format.wrapAttributes"] = "aligned-multiple" ]]
 nvim_lsp.html.setup({
-	cmd = { "vscode-html-language-server", "--stdio" },
-	settings = {
-		html = {
-			format = {
-				wrapLineLength = 180,
-				wrapAttributes = "aligned-multiple",
-			},
-		},
-	},
-	filetypes = { "html", "templ" },
-	init_options = {
-		configurationSection = { "html", "css" },
-		embeddedLanguages = {
-			html = true,
-			css = true,
-			javascript = false,
-		},
-		provideFormatter = true,
-	},
-	on_attach = lsp.on_attach,
-	capabilities = lsp.capabilities,
+    cmd = { "vscode-html-language-server", "--stdio" },
+    settings = {
+        html = {
+            format = {
+                wrapLineLength = 180,
+                wrapAttributes = "aligned-multiple",
+            },
+        },
+    },
+    filetypes = { "html", "templ" },
+    init_options = {
+        configurationSection = { "html", "css" },
+        embeddedLanguages = {
+            html = true,
+            css = true,
+            javascript = false,
+        },
+        provideFormatter = true,
+    },
+    on_attach = lsp.on_attach,
+    capabilities = lsp.capabilities,
 })
 
 -- servers that lspconfig supports but mason doesn't have
 nvim_lsp.ccls.setup({
-	on_attach = lsp.on_attach,
-	capabilities = lsp.capabilities,
+    on_attach = lsp.on_attach,
+    capabilities = lsp.capabilities,
 })
 
 local pid = vim.fn.getpid()
 local mason_path = vim.fn.stdpath("data") .. "/mason/bin"
 nvim_lsp.omnisharp.setup({
-	handlers = {
-		["textDocument/definition"] = require("omnisharp_extended").handler,
-	},
-	cmd = { mason_path .. "/omnisharp", "--languageserver", "--hostPID", tostring(pid) },
-	-- Enables support for reading code style, naming convention and analyzer
-	-- settings from .editorconfig.
-	enable_editorconfig_support = true,
+    handlers = {
+        ["textDocument/definition"] = require("omnisharp_extended").handler,
+    },
+    cmd = { mason_path .. "/omnisharp", "--languageserver", "--hostPID", tostring(pid) },
+    -- Enables support for reading code style, naming convention and analyzer
+    -- settings from .editorconfig.
+    enable_editorconfig_support = true,
 
-	-- If true, MSBuild project system will only load projects for files that
-	-- were opened in the editor. This setting is useful for big C# codebases
-	-- and allows for faster initialization of code navigation features only
-	-- for projects that are relevant to code that is being edited. With this
-	-- setting enabled OmniSharp may load fewer projects and may thus display
-	-- incomplete reference lists for symbols.
-	enable_ms_build_load_projects_on_demand = false,
+    -- If true, MSBuild project system will only load projects for files that
+    -- were opened in the editor. This setting is useful for big C# codebases
+    -- and allows for faster initialization of code navigation features only
+    -- for projects that are relevant to code that is being edited. With this
+    -- setting enabled OmniSharp may load fewer projects and may thus display
+    -- incomplete reference lists for symbols.
+    enable_ms_build_load_projects_on_demand = false,
 
-	-- Enables support for roslyn analyzers, code fixes and rulesets.
-	enable_roslyn_analyzers = false,
+    -- Enables support for roslyn analyzers, code fixes and rulesets.
+    enable_roslyn_analyzers = false,
 
-	-- Specifies whether 'using' directives should be grouped and sorted during
-	-- document formatting.
-	organize_imports_on_format = false,
+    -- Specifies whether 'using' directives should be grouped and sorted during
+    -- document formatting.
+    organize_imports_on_format = false,
 
-	-- Enables support for showing unimported types and unimported extension
-	-- methods in completion lists. When committed, the appropriate using
-	-- directive will be added at the top of the current file. This option can
-	-- have a negative impact on initial completion responsiveness,
-	-- particularly for the first few completion sessions after opening a
-	-- solution.
-	enable_import_completion = false,
+    -- Enables support for showing unimported types and unimported extension
+    -- methods in completion lists. When committed, the appropriate using
+    -- directive will be added at the top of the current file. This option can
+    -- have a negative impact on initial completion responsiveness,
+    -- particularly for the first few completion sessions after opening a
+    -- solution.
+    enable_import_completion = false,
 
-	-- Specifies whether to include preview versions of the .NET SDK when
-	-- determining which version to use for project loading.
-	sdk_include_prereleases = true,
+    -- Specifies whether to include preview versions of the .NET SDK when
+    -- determining which version to use for project loading.
+    sdk_include_prereleases = true,
 
-	-- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-	-- true
-	analyze_open_documents_only = false,
+    -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+    -- true
+    analyze_open_documents_only = false,
 })
