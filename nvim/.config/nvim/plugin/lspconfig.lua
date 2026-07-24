2a22ff582601f2c2bfe1777a37f08abb193ef2bd
Kevin_Tung <Kevin.Tung@ibm.com>
2026-07-24 21:13:59 +0800

    modified

--- nvim/.config/nvim/plugin/lspconfig.lua
+++ nvim/.config/nvim/plugin/lspconfig.lua
@@ -256,6 +256,18 @@ local vtsls_config = {
 }
 
 local ts_ls_config = {
+    root_dir = function(bufnr, on_dir)
+        local root = vim.fs.root(bufnr, {
+            "angular.json",
+            "tsconfig.json",
+            "jsconfig.json",
+            "package.json",
+            ".git",
+        })
+        local filename = vim.api.nvim_buf_get_name(bufnr)
+
+        on_dir(root or vim.fs.dirname(filename))
+    end,
     init_options = {
         hostInfo = "neovim",
         plugins = {
