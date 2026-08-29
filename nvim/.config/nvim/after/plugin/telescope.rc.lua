fcd39f4375ab6db99c57e93832942de69b16d869
Kevin_Tung <Kevin.Tung@ibm.com>
2026-08-29 16:27:22 +0800

    feat: added telescope config

--- nvim/.config/nvim/after/plugin/telescope.rc.lua
+++ nvim/.config/nvim/after/plugin/telescope.rc.lua
@@ -49,7 +49,11 @@ require("telescope").setup({
         -- Developer configurations: Not meant for general override
         buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
         mappings = {
-            n = { ["q"] = require("telescope.actions").close },
+            n = {
+                ["q"] = require("telescope.actions").close,
+                [","] = require("telescope.actions").results_scrolling_up,
+                ["."] = require("telescope.actions").results_scrolling_down,
+            },
         },
     },
     extensions_list = { "themes", "terms" },
